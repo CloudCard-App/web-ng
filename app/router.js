329846b48f65e94b504867f6e7c7d295b543ca19
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-08-04 22:38:38 -0700

    Add proper dashboard for teacher

--- app/router.js
+++ app/router.js
@@ -21,7 +21,36 @@ module.exports = function (passport, app) {
 
   app.get('/teacher/dashboard', isLoggedIn, (req, res) => {
     teacher.getClassList(req.user._id).then((classes) => {
-      res.render('teacher/dashboard', {'user.info': req.user, 'user.classes': classes});
+      console.log('rendering classes: ' + classes);
+      res.render('teacher/dashboard', {
+        'user': {
+          'info': req.user,
+          'classes': classes
+        }
+      });
+    });
+  });
+
+  app.post('/teacher/createClass', isLoggedIn, (req, res) => {
+    teacher.createClass(req.user._id, req.body.name, req.body.password).then((newClass) => {
+      res.redirect('/teacher/dashboard');
+    }).catch((error) => {
+    });
+  });
+
+  app.get('/teacher/class/*', isLoggedIn, (req, res) => {
+    let classID = req.query.id;
+    console.log('classid = ' + classID);
+    res.render('teacher/class', {
+      className: 'Test Class', decks: [
+        {name: 'Deck 1'},
+        {name: 'Deck 2'},
+        {name: 'Deck 3'},
+        {name: 'Deck 4'},
+        {name: 'Deck 5'},
+        {name: 'Deck 6'},
+        {name: 'Deck 7'}
+      ]
     });
   });
 
