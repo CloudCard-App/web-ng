0fec1e3ca2820fce5ea08b8439d89a99bee63adb
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-08-09 22:39:37 -0700

    Fix HTML encoding/decoding issues

--- app/routes/student/deck.js
+++ app/routes/student/deck.js
@@ -1,5 +1,4 @@
 let deckModel = require('../../models/deck');
-let entities = require('entities');
 
 module.exports.study = (req, res) => {
   let deckID = req.query.deckID;
@@ -9,26 +8,11 @@ module.exports.study = (req, res) => {
 
   Promise.all([deckInfoQuery, cardsQuery]).then((results) => {
     let deckInfo = results[0];
-    let cardListEncoded = results[1];
-
-    let cardListDecoded = [];
-
-    cardListEncoded.forEach((deck) => {
-
-      let decodedFront = entities.decodeHTML(deck.front);
-      let decodedBack = entities.decodeHTML(deck.back);
-      console.log('decoded front ' + decodedFront);
-      console.log('decoded back ' + decodedBack);
-      cardListDecoded.push({
-        front: decodedFront,
-        back: decodedBack,
-        datatype: deck.back
-      });
-    });
+    let cardList = results[1];
 
     res.render('student/study', {
       deckInfo: deckInfo,
-      cardList: cardListDecoded,
+      cardList: cardList,
       classInfo: deckInfo.class
     })
   })
