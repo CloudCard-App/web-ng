CloudCard-App/web-ng

commit 93c046df6ce89319e5b6e3a2bfe265c2effa4140

Enhance login page
If a school is not registered, the student or teacher attempting to sign in will get an error message telling them to use their school email address.

app/config/auth/passport.js
@@ -8,17 +8,15 @@ module.exports = function (passport) {
   passport.serializeUser(function (user, done) {
     console.log('serializing user: ' + user);
     let userID = JSON.parse(user)._id;
-    console.log('userID = ' + userID);
     done(null, userID); //Do whatever is next with the user.id
   });
 
   // used to deserialize the user, finding based on id
   passport.deserializeUser(function (id, done) {
-    console.log('deserializing user!');
     request(process.env.CORE_URL + '/student/info/?id=' + id, function (error, response, body) {
       if (!error && response.statusCode == 200 && body) {
         console.log('deserialized student: ' + body);
-        done(null, JSON.parse(body));
+        done(null, null);
       } else {
         request(process.env.CORE_URL + '/teacher/info/?id=' + id, function (error, response, body) {
           if (!error && response.statusCode == 200) {
@@ -48,9 +46,12 @@ module.exports = function (passport) {
             gavatar: profile.profileUrl
           }
         }, function (error, response, body) {
-          console.log('new teacher: ' + body);
-          console.log('response: ' + response);
-          return done(error, body);
+          if (JSON.parse(body).error) { // School not found
+            console.log('done with school not found');
+            return done(null, false, body);
+          } else {
+            return done(null, body);
+          }
         });
       });
     }
@@ -73,8 +74,12 @@ module.exports = function (passport) {
         }
       }, function (error, response, body) {
         console.log('new student: ' + body);
-        console.log('response: ' + response);
-        return done(error, body);
+        if (JSON.parse(body).error) { // School not found
+          console.log('done with school not found');
+          return done(null, false, body);
+        } else {
+          return done(null, body);
+        }
       });
     });
   }));

app/router.js
@@ -10,7 +10,8 @@ let studentDeckRoutes = require('./routes/student/deck');
 
 module.exports = function (passport, app) {
   app.get('/', function (req, res) {
-    res.render('index');
+    console.log('req.query = ' + req.query.err);
+    res.render('index', {error: req.query.err});
   });
 
   app.get('/auth/google/teacher', passport.authenticate('teacher', {scope: ['profile', 'email']}));
@@ -24,12 +25,16 @@ module.exports = function (passport, app) {
 
   app.get('/auth/google/callback/teacher', passport.authenticate('teacher', {
     successRedirect: '/teacher/dashboard',
-    failureRedirect: '/'
+    failureRedirect: '/?err=schoolNotRegistered',
+    session: false,
+    failureFlash: true
   }));
 
   app.get('/auth/google/callback/student', passport.authenticate('student', {
     successRedirect: '/student/dashboard',
-    failureRedirect: '/'
+    failureRedirect: '/?err=schoolNotRegistered',
+    session: false,
+    failureFlash: true
   }));
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- TEACHER -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -40,7 +45,7 @@ module.exports = function (passport, app) {
 
   app.get('/teacher/class/class/*', isLoggedIn, teacherClassRoutes.class);
 
-  app.post('/teacher/class/deleteClass', isLoggedIn, teacherClassRoutes.deleteClass)
+  app.post('/teacher/class/deleteClass', isLoggedIn, teacherClassRoutes.deleteClass);
 
   app.post('/teacher/deck/createDeck', isLoggedIn, teacherDeckRoutes.createDeck);
 

commit 5cf5aa27c67615cbe75d5801dc339214800e625a

Fix login issues
Fixes issues introduced by commit 93c046df6ce89319e5b6e3a2bfe265c2effa4140

app/config/auth/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
   // used to serialize the user for the session
   passport.serializeUser(function (user, done) {
     console.log('serializing user: ' + user);
-    let userID = JSON.parse(user)._id;
+    let userID = user._id;
     done(null, userID); //Do whatever is next with the user.id
   });
 
@@ -16,7 +16,7 @@ module.exports = function (passport) {
     request(process.env.CORE_URL + '/student/info/?id=' + id, function (error, response, body) {
       if (!error && response.statusCode == 200 && body) {
         console.log('deserialized student: ' + body);
-        done(null, null);
+        done(null, JSON.parse(body));
       } else {
         request(process.env.CORE_URL + '/teacher/info/?id=' + id, function (error, response, body) {
           if (!error && response.statusCode == 200) {
@@ -50,7 +50,7 @@ module.exports = function (passport) {
             console.log('done with school not found');
             return done(null, false, body);
           } else {
-            return done(null, body);
+            return done(null, JSON.parse(body));
           }
         });
       });
@@ -78,7 +78,7 @@ module.exports = function (passport) {
           console.log('done with school not found');
           return done(null, false, body);
         } else {
-          return done(null, body);
+          return done(error, JSON.parse(body));
         }
       });
     });

app/router.js
@@ -26,15 +26,11 @@ module.exports = function (passport, app) {
   app.get('/auth/google/callback/teacher', passport.authenticate('teacher', {
     successRedirect: '/teacher/dashboard',
     failureRedirect: '/?err=schoolNotRegistered',
-    session: false,
-    failureFlash: true
   }));
 
   app.get('/auth/google/callback/student', passport.authenticate('student', {
     successRedirect: '/student/dashboard',
     failureRedirect: '/?err=schoolNotRegistered',
-    session: false,
-    failureFlash: true
   }));
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- TEACHER -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -79,9 +75,11 @@ module.exports = function (passport, app) {
 
 function isLoggedIn(req, res, next) {
   // TODO: if you're logged in you can access any of the other (teacher vs. student) paths as well
+  console.log('user = ' + req.user);
   if (req.isAuthenticated()) {
     return next();
   } else {
+    console.log('not authenticated: redirecting to home');
     res.redirect('/');
   }
 }